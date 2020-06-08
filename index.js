b1b60ba89c89badde24d90f4e30b4d81b7da35b7
Mila1444 <66134690+Mila1444@users.noreply.github.com>
2020-06-07 22:40:36 -0400

    Updating Project

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ function ask(questionText) {
   });
 }
 
+// Original random function
+// function randomNum(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min)
+// }
+
+// binary search function
 function randomNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor((min + max) / 2)
 }
 
 start();
@@ -17,8 +23,15 @@ start();
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
+  //console.log('You entered: ' + secretNumber); (given with original code)
   // Wait for user to think of a number and ask if random number is correct
+  let maxRange = await ask("But first, let's set your range! from 1 to ...? (Enter a number greater than 1)")
+  // making sure the user enters an actual number
+  if (isNaN(maxRange)) {
+    maxRange = await ask("That's not a valid response! Give me an actual number! (Enter a number greater than 1)")
+  }
+  let maxR = parseInt(maxRange)
+  maxNum = maxR
   let randomNumber = randomNum(minNum, maxNum)
   let answer = await ask("Is this your number?\n" + randomNumber + " Yes(y) or No(n)")
 
@@ -26,29 +39,36 @@ async function start() {
     console.log(`Your number was ${randomNumber}! I guessed it!`)
     process.exit();
   } if (answer.toLowerCase() !== "n") {
-    console.log("That's not an valid answer. Let's start over!")
-    process.exit()
+    answer = await ask(`That's not an valid answer. Was ${randomNumber} your secret number? Yes(y) or No(n)`)
   }
 
   while (answer.toLowerCase() === "n") {
-    let highOrLow = await ask("Is it higher (H) or lower (L)?") 
-    // let highNum = randomNum(newMin, maxNum)
-    // let lowNum = randomNum(minNum, newMax)
+    let highOrLow = await ask("Is it higher (H) or lower (L)?")
     if (highOrLow.toLowerCase() === "h") {
       minNum = randomNumber + 1
       randomNumber = randomNum(minNum, maxNum)
       answer = await ask("Is this your number?\n" + randomNumber + " Yes(y) or No(n)")
-      console.log(answer)
+      //making sure user is not cheating
+      if (randomNumber === minNum) {
+        console.log("Your secret number can't be any higher. You are cheating! Good bye!")
+        process.exit()
+      }
     } else if (highOrLow.toLowerCase() === "l") {
       maxNum = randomNumber - 1
       randomNumber = randomNum(minNum, maxNum)
       answer = await ask("Is this your number?\n" + randomNumber + " Yes(y) or No(n)")
-    } else {
+      //making sure user is not cheating
+      if (randomNumber === maxNum) {
+        console.log("Your secret number can't be any lower. You are cheating! Good bye!")
+        process.exit()
+      }
+      // in case the user enter something that's is not "h"/"l"
+    } else if (answer.toLowerCase() !== "h" || "l") {
       console.log("That's not an valid answer. Let's start over!")
       process.exit()
     }
   }
-  console.log(`Your number was ${randomNumber}! I guessed it in X tries! Bye now!`) // try to define X
-    process.exit()
+  console.log(`Your number was ${randomNumber}! That was fun! Wasn't it? Bye, bye!`) // Ask about how I could get the actual number of tries
+  process.exit()
 }
 
